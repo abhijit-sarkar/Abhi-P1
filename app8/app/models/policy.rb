9867427ada055fe5f2bb1a543d8f761d80ee800a
Abhijit <abhijits30@gmail.com>
2016-12-27 12:35:16 +0530

    Revert "Insurance Policy Management(Extension)"
    This reverts commit 3304d0685681d35a95a07cbd5777ab14e7dd0dbe.

--- app8/app/models/policy.rb
+++
@@ -1,8 +0,0 @@
-class Policy < ApplicationRecord
-  belongs_to :customer
-  validates :plan_id,presence: true
-  validates :customer_id,presence: true
-  validates :sum_insured,presence: true
-  validates :duration,presence: true,:exclusion => {:in =>0..0,:message => "cannot be zero"}
-  validates :nominee,presence: true
-end
