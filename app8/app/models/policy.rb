0f3c1ddc57682318a9e0547f0f63252bb9769eb5
Abhijit <abhijits30@gmail.com>
2016-12-29 13:17:13 +0530

    Revert "Inrance Policy Management(Extension)"
    This reverts commit 619e9cd5c7d8c800efd279a1f58c7dbcae5bd0f0.

--- app8/app/models/policy.rb
+++
@@ -1,9 +0,0 @@
-class Policy < ApplicationRecord
-  belongs_to :customer
-  has_many :policy_documents,dependent: :destroy
-  validates :plan_id,presence: true
-  validates :customer_id,presence: true
-  validates :sum_insured,presence: true
-  validates :duration,presence: true,:exclusion => {:in =>0..0,:message => "cannot be zero"}
-  validates :nominee,presence: true
-end
--- app8/app/models/policy_document.rb
+++
@@ -1,5 +0,0 @@
-class PolicyDocument < ApplicationRecord
-  belongs_to :policy
-  has_attached_file :image, styles: { }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-end
