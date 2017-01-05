6c50b17b10969b17ed0b74459f2c39a3ae1a698c
Abhijit <abhijits30@gmail.com>
2017-01-05 23:19:27 +0530

    Revert "Insurance Policy Management(Extension)"
    This reverts commit 01c434f6d1f4e3657376db68d96400bb420b885d.

--- app8/app/models/customer.rb
+++
@@ -1,9 +0,0 @@
-class Customer < ApplicationRecord
-  has_many :policies,dependent: :destroy
-  has_many :comments,dependent: :destroy
-  validates :customer_name,presence: true
-  validates :contact,presence: true,numericality: true, length: {minimum: 10, maximum: 10,:message=> "is too short(minimum is 10 digits)"}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email,presence: true,format: {with: VALID_EMAIL_REGEX}
-
-end
