0343297ffe0d0a4afc7d3c8e4efaec8b32a6577b
Abhijit <abhijits30@gmail.com>
2017-01-31 10:48:25 +0530

    Insurance Policy Management

---
+++ app8/config/routes.rb
@@ -0,0 +1,49 @@
+Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    root to: 'home#index', as: :authenticated_root
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :companies   
+  resources :comments  
+  resources :home 
+  
+  resources :policy_documents do
+    member do
+      get "list"		
+    end
+  end
+  
+  resources :policies do
+    member do
+		get "policy_list"	
+		post "policy_details"
+		post "policy_view"
+		post "check_data"	
+		get "cancel_policy"
+		get "send_mail"
+		get "policy_details"	
+	end
+  end
+
+  resources :customers do
+    member do
+      get "surrender"
+	  get "send_mail"
+    end
+  end
+  
+  resources :transactions do
+    member do
+      get "send_mail"		
+    end
+  end
+
+  resources :plans do
+    member do
+      get "list"		
+    end
+  end
+ 
+  root 'devise/sessions#new'
+end
---
+++ app8/db/migrate/20161203064854_create_plans.rb
@@ -0,0 +1,15 @@
+class CreatePlans < ActiveRecord::Migration[5.0]
+  def change
+    create_table :plans do |t|
+      t.string :plan_name , null: false
+      t.integer :max_tenure , null: false
+      t.integer :interest , null: false
+      t.integer :bonus , null: false
+      t.integer :min_amount , null: false
+      t.integer :max_amount , null: false
+      t.references :company , foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
