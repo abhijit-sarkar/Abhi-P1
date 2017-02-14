8a7826daa51dc108e08e470f4969cac3fe6de607
Abhijit <abhijits30@gmail.com>
2017-02-14 11:46:47 +0530

    Revert "Insurance Policy Management"
    This reverts commit c6e98904fb8b1c10296329a92b8700d2e91605a8.

--- app8/app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-
-  def show
-    session[:pid] = params[:id]
-    session[:call] = "home"
-    @pols = Policy.all    
-  end
-  
-end
--- app8/app/mailers/application_mailer.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: 'sarkar.abhijit9119@gmail.com'
-  layout 'mailer'
-end
