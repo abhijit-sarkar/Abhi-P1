c09c1526a71fd3f9a704fa5b0b41d635f3b139ad
Abhijit <abhijits30@gmail.com>
2017-02-20 11:06:36 +0530

    Revert "Insurance Policy Management(Extension)"
    This reverts commit 51bf0a2ceee2f1fdd7aeca016496ccdb987a4ec7.

--- app8/db/migrate/20161226174909_create_policy_documents.rb
+++
@@ -1,9 +0,0 @@
-class CreatePolicyDocuments < ActiveRecord::Migration[5.0]
-  def change
-    create_table :policy_documents do |t|
-      t.string :title 
-	  t.references :policy, foreign_key: true 
-      t.timestamps
-    end
-  end
-end
